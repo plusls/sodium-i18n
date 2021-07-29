8057cb744907f5d429f931654983e412315d87c3
plulsls <plusls@qq.com>
2021-07-30 01:33:38 +0800

    fix translation in OptionImpact

--- src/main/java/com/plusls/sodium_i18n/mixin/MixinOptionImpact.java
+++ src/main/java/com/plusls/sodium_i18n/mixin/MixinOptionImpact.java
@@ -2,14 +2,20 @@ package com.plusls.sodium_i18n.mixin;
 
 import me.jellysquid.mods.sodium.client.gui.options.OptionImpact;
 import net.minecraft.client.resource.language.I18n;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = OptionImpact.class, remap = false)
 public class MixinOptionImpact {
-    @ModifyVariable(method = "<init>", at = @At(value = "HEAD"), ordinal = 0)
-    private static String i18nText(String text) {
+    @Final
+    @Shadow
+    private String text;
+    @Redirect(method = "toDisplayString", at = @At(value = "FIELD", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionImpact;text:Ljava/lang/String;", ordinal = 0))
+    private String i18nText(OptionImpact optionImpact) {
         return I18n.translate(text);
     }
 }
